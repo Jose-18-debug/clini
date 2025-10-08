9b3fdcb5499e1c6a3ffc5b859b6a09278e65bd26
José María <sa546357@uaeh.edu.mx>
2025-10-07 23:51:42 -0600

    Hola

--- OneDrive/Documents/Programas Python/ejemploDeSumatoria.py
+++ OneDrive/Documents/Programas Python/ejemploDeSumatoria.py
@@ -1,8 +1,16 @@
 n = int(input("¿Cuántos valores vas a calcular?: "))
-vector1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+vector1 = []
+for i in range (n):
+    valores = int(input("Ingresa los valores del vector: "))
+    vector1.append(valores)
 pos = 0
 ac = 0
 while (pos < n):
     ac = ac + vector1[pos]
     pos = pos + 1
 print("La sumatoria es: ", ac)
+
+if (ac%2 == 0):
+    print("Es par")
+else:
+    print("Es impar")
